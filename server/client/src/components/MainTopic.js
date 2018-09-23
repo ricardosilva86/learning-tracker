04c9d8c8b4ad96ec4cdfdc90cafc4f9a1808c385
Ricardo da Silva <ricardolopes18@yahoo.com.br>
2018-09-23 12:56:28 +0200

    Finalizando client side

--- server/client/src/components/MainTopic.js
+++ server/client/src/components/MainTopic.js
@@ -18,12 +18,19 @@ class MainTopic extends Component {
   }
   render() {
       const dados = this.state.matter;
+
     return (
       dados.map(function (dado) {
+          let progressBarValues = {
+              width: dado.percentage_concluded
+          }
           return (
               <div className="card mb-3">
                   <div className="card-header">
-                      <h4 className="card-title" ><a className="btn btn-info" data-toggle="collapse" href={"#collapse-"+dado._id} role="button" aria-expanded="false" aria-controls={"collapse-"+dado._id}>{dado.title}</a></h4>
+                      <h4 className="card-title" ><a className="btn btn-dark" data-toggle="collapse" href={"#collapse-"+dado._id} role="button" aria-expanded="false" aria-controls={"collapse-"+dado._id}>{dado.title}</a></h4>
+                      <div className="progress">
+                          <div className="progress-bar" role="progressbar" style={progressBarValues} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
                   </div>
                   <div className="card-body collapse" id={"collapse-"+dado._id}>
                       <Topic topic={dado.topics} />
