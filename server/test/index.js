fc57f74a816194c42a232508c1366b21015ef466
Ricardo da Silva <ricardolopes18@yahoo.com.br>
2018-09-23 22:37:45 +0200

    Adicionando testes

---
+++ server/test/index.js
@@ -0,0 +1,18 @@
+process.env.NODE_ENV = 'test';
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+const app = require('../app');
+
+chai.use(chaiHttp);
+
+describe('First Test', () => {
+   it('Application test', (done) => {
+      chai.request(app)
+          .get('/api')
+          .end((err, res) => {
+              res.should.have.status(200);
+              done();
+          })
+   })
+});
