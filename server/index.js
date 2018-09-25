ff4a04d3360a81618a04c7147e360915e7ec3b37
Ricardo da Silva <ricardolopes18@yahoo.com.br>
2018-09-25 20:52:38 +0200

    Removed JSHint and added ESLint

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const app = require('./app');
+const app = require("./app");
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+	console.log(`Running on port ${PORT}`);
 });
