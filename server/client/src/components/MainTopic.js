4f2f52ef78983de240124d434948a3dba68fffa6
Ricardo da Silva <ricardolopes18@yahoo.com.br>
2019-08-16 21:53:41 +0200

    Fix issues with linting

--- server/client/src/components/MainTopic.js
+++ server/client/src/components/MainTopic.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import Topic from "./Topic";
 import $ from "jquery";
-import axios from 'axios';
+import axios from "axios";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faEye } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTrash, faEye } from "@fortawesome/free-solid-svg-icons"
 
 library.add(faTrash, faEye);
 
@@ -13,7 +13,7 @@ class MainTopic extends Component {
 	constructor() {
 		super();
 		this.onDelete = this.onDelete.bind(this);
-        this.onEdit = this.onEdit.bind(this);
+		this.onEdit = this.onEdit.bind(this);
 		this.state = { matter: [] };
 	}
 	componentDidMount(){
@@ -25,56 +25,56 @@ class MainTopic extends Component {
 			}.bind(this)
 		});
 	}
-    onDelete(dado){
+	onDelete(dado){
 		axios.delete(`/api/maintopic/${dado._id}`)
 			.then(
-                axios.get("/api/all").then((dados) => this.setState({matter: dados}))
-			)
+				axios.get("/api/all").then((dados) => this.setState({matter: dados}))
+			);
 	}
 
-    onEdit(dado){
-		console.log(dado._id);
-    };
+	onEdit(dado){
+		//console.log(dado._id);
+	}
 	render() {
 		const dados = this.state.matter;
 		if (dados.length > 0) {
-            return (
-                dados.map(dado => {
-                    let progressBarValues = {
-                        width: dado.percentage_concluded
-                    };
-                    return (
-                        <div key={dado._id} className="card mb-3">
-                            <div className="card-header">
-                                <span className="btn btn-light float-lg-right"
-                                      onClick={() => this.onDelete(dado)}><FontAwesomeIcon icon="trash"/></span><span
-                                className="btn btn-light float-lg-right"
-                                onClick={() => this.onEdit(dado)}><FontAwesomeIcon icon="eye"/></span>
-                                <h4 className="card-title"><a className="btn btn-dark" data-toggle="collapse"
-                                                              href={"#collapse-" + dado._id} role="button"
-                                                              aria-expanded="false"
-                                                              aria-controls={"collapse-" + dado._id}>{dado.title}</a>
-                                </h4>
-                                <div className="progress">
-                                    <div className="progress-bar" role="progressbar" style={progressBarValues}
-                                         aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </div>
-                            <div className="card-body collapse" id={"collapse-" + dado._id}>
-                                <Topic topic={dado.topics}/>
-                            </div>
-                        </div>
-                    );
-                })
-            );
-        }else{
-            return (
-                <div className="card mb-3">
+			return (
+				dados.map(dado => {
+					let progressBarValues = {
+						width: dado.percentage_concluded
+					};
+					return (
+						<div key={dado._id} className="card mb-3">
+							<div className="card-header">
+								<span className="btn btn-light float-lg-right"
+									onClick={() => this.onDelete(dado)}><FontAwesomeIcon icon="trash"/></span><span
+									className="btn btn-light float-lg-right"
+									onClick={() => this.onEdit(dado)}><FontAwesomeIcon icon="eye"/></span>
+								<h4 className="card-title"><a className="btn btn-dark" data-toggle="collapse"
+									href={"#collapse-" + dado._id} role="button"
+									aria-expanded="false"
+									aria-controls={"collapse-" + dado._id}>{dado.title}</a>
+								</h4>
+								<div className="progress">
+									<div className="progress-bar" role="progressbar" style={progressBarValues}
+										aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+							</div>
+							<div className="card-body collapse" id={"collapse-" + dado._id}>
+								<Topic topic={dado.topics}/>
+							</div>
+						</div>
+					);
+				})
+			);
+		}else{
+			return (
+				<div className="card mb-3">
 					<div className="card-header">
 						<h4 className="card-title">No Topic found</h4>
 					</div>
 				</div>
-			)
+			);
 		}
 	}
 
