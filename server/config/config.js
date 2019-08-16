d0bef92aaca14329bb1eee23aa183ed8f0a9c6c2
Ricardo da Silva <ricardolopes18@yahoo.com.br>
2019-08-16 21:29:59 +0200

    Add cloud.mongodb url

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,7 @@
 const passwd = process.env.MONGO_PASSWD;
+const passwdAtlas = process.env.MONGO_PASSWD_ATLAS;
 
 module.exports = {
+	//mongoConnectionString: `mongodb+srv://admin:${passwdAtlas}@learning-tracker-7puau.gcp.mongodb.net/test?retryWrites=true&w=majority`
 	mongoConnectionString: `mongodb://admin:${passwd}@ds131743.mlab.com:31743/learning-tracker`
 };
