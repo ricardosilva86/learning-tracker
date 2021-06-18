d84d51e37c9529863a921366e4a29673551a8bfc
Ricardo Silva <r.lopesdasilva@yahoo.com.br>
2021-06-18 09:12:10 +0200

    make app more parameterized

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,10 @@
-const passwd = process.env.MONGO_PASSWD;
+const mongoServer = process.env.MONGO_DB_URL; //ds131743.mlab.com:31743
+const mongoDbName = process.env.MONGO_DB_NAME; //learning-tracker
+const mongoDbUser = process.env.MONGO_DB_USER; //admin
+const mongoDBpass = process.env.MONGO_PASSWD;
 //const passwdAtlas = process.env.MONGO_PASSWD_ATLAS;
 
 module.exports = {
 	//mongoConnectionString: `mongodb+srv://admin:${passwdAtlas}@learning-tracker-7puau.gcp.mongodb.net/test?retryWrites=true&w=majority`
-	mongoConnectionString: `mongodb://admin:${passwd}@ds131743.mlab.com:31743/learning-tracker`
+	mongoConnectionString: `mongodb://${mongoDbUser}:${mongoDBpass}@${mongoServer}/${mongoDbName}`
 };
